b60a55cac52deb62cffbfb68170a6cba53603946
shivshankar-112 <santosingh505s@gmail.com>
2026-06-24 19:07:15 +0530

    Update MongoDB connection URI and database name

--- src/utils/dbConnect.ts
+++ src/utils/dbConnect.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const monogo_uri:string = process.env.MONGO_URI || 'mongodb://admin:lotus@allacess-@ac-k5rvmvl-shard-00-00.a7eiv19.mongodb.net:27017,ac-k5rvmvl-shard-00-01.a7eiv19.mongodb.net:27017,ac-k5rvmvl-shard-00-02.a7eiv19.mongodb.net:27017/game?ssl=true&replicaSet=atlas-32hq2y-shard-0&authSource=admin&appName=Cluster0'
+const monogo_uri:string = process.env.MONGO_URI || 'mongodb://admin:lotus@allacess-@ac-k5rvmvl-shard-00-00.a7eiv19.mongodb.net:27017,ac-k5rvmvl-shard-00-01.a7eiv19.mongodb.net:27017,ac-k5rvmvl-shard-00-02.a7eiv19.mongodb.net:27017/lotus-game?ssl=true&replicaSet=atlas-32hq2y-shard-0&authSource=admin&appName=Cluster0'
 console.log("Mongo URI:", monogo_uri);
 export const connectDb = async () => {
     try {
